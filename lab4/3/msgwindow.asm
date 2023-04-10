e27cdffdfd8582b588c22492cc9931bbc62ab294
AlexGrama22 <alexgrama837@gmail.com>
2023-04-10 03:06:18 +0300

    lab4

---
+++ lab4/3/msgwindow.asm
@@ -0,0 +1,30 @@
+NULL EQU 0
+MB_DEFBUTTON EQU 100h
+IDNO EQU 7
+MB_YESNO EQU 4
+
+extern _MessageBoxA@16
+extern _ExitProcess@4
+
+global Start
+
+section .data
+
+MessageBoxMessage db "Sunteti multumitit de caliatatea drumurilor din Moldova?", 0
+MessageBoxTitle db "Chestionar", 0
+
+section .text
+
+Start:
+
+push MB_YESNO | MB_DEFBUTTON
+push MessageBoxTitle
+push MessageBoxMessage
+push NULL
+call _MessageBoxA@16
+
+cmp EAX, IDNO
+je Start
+
+push NULL
+call _ExitProcess@4
